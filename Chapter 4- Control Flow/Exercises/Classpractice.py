47105bbbed035464b976b6d9d0f5ea2dd35bf2c0
blackswordsman01 <144208181+blackswordsman01@users.noreply.github.com>
2023-10-26 19:56:42 +0400

    Classpractice.py

--- Chapter 4- Control Flow/Exercises/Classpractice.py
+++ Chapter 4- Control Flow/Exercises/Classpractice.py
@@ -17,3 +17,17 @@ if classmarks > 70:
         print("Your classmarks is not sufficient")
 else:
     print("Your externals is not sufficient")
+
+#elif statements
+classpoints = float(input ("Enter your classpoints"))
+if classpoints >= 100:
+    print ("Your class is D")
+elif classpoints >= 300:
+    print ("Your class is C")
+elif classpoints >= 500:
+    print ("Your class is B")
+elif classpoints >= 700:
+    print ("Your class is A")
+else :
+    print ("You are expelled")
+    
